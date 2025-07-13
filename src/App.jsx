def6456aec45a1a63a70598e85e7f88f3d63f0d9
Ayush <imayush19052003@gmail.com>
2025-07-13 17:11:59 +0530

    sign in and sign up form in same form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {``
 
   return (
     <>
-      <div className="font-bold">Hii</div>
+      <Body />
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInform = () => {
+    console.log("sign in form value", isSignInForm);
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header></Header>
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_medium.jpg"
+          alt="background"
+        />
+      </div>
+      <form className=" w-4/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-black/80 ">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-800"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-800"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-800"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded- lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInform}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up now"
+            : "Already a user. Sign In now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
